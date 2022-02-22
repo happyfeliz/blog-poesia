d6abdde49e3c92c59e79c59c648b26e48979c7cf
happyfeliz <75329205+happyfeliz@users.noreply.github.com>
2022-02-22 07:19:45 -0600

    Pruebas :(

--- blog/src/index.js
+++ blog/src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementsByTagNameNS('body')
+  document.getElementsById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
