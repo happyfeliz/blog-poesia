6e053f66367b6c97299cd6f5d0f6eda110ff2c50
happyfeliz <75329205+happyfeliz@users.noreply.github.com>
2022-02-22 12:35:35 -0600

    Primera parte

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {useState} from "react"
 import './App.css';
 import Surprise from "./Principal_Nav";
+import Images from "./Principal_Background"
 function App() {
   const [showSurprise, setShowSurprise] = useState(false)
   return(
-    <div>
-      <button onClick={(ev)=> setShowSurprise(!showSurprise)}>
+    <div id="MenuContainer">
+      <button id="MenuBtn" onClick={(ev)=> setShowSurprise(!showSurprise)}>
         Menu
       </button>
+      <Images/>
       {showSurprise && <Surprise />}
     </div>)
 }
---
+++ src/Principal_Background.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default (props) => {
+    return(
+        <container>
+            <aside>
+                <img src="Git\static\Images\Rey.png"/>
+            </aside>
+            <aside> 
+                <img/>
+            </aside>
+        </container>
+    )
+}
